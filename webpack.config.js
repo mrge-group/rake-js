873429bd75b5d5db8cbd6f2491222776e08dcd05
David Kurt <david.kurt@s24.com>
2019-09-12 09:54:19 +0200

    Installed jest and eslint with draft config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    output: {
+        filename: 'dist/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
