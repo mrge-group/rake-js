e21b7971b824761606b7ffd564f7c9c898fdea15
Kay Gensmann <kay.gensmann@s24.com>
2019-09-18 17:13:10 +0200

    more testing

--- tests/unit/splitbystopwords.test.js
+++ tests/unit/splitbystopwords.test.js
@@ -3,10 +3,17 @@ import { splitByStopWords } from '../../src/keywords'
 
 describe('split by stop words', () => {
     const stopWords = ['eins', 'fünf', 'neun']
-    const sentences = ['eins zwei drei vier fünf sechs sieben acht neun zehn', 'eins eins eins vier']
+    let sentences = ['eins zwei drei vier fünf sechs sieben acht neun zehn', 'eins eins eins vier']
     const splitWords = splitByStopWords(sentences, stopWords)
 
     it('should return phrases including whitespaces and removed places', () => {
         expect(splitWords).toEqual(['', ' zwei drei vier ', ' sechs sieben acht ', ' zehn', '', ' ', ' ', ' vier'])
+    })
+
+    sentences = ['zwei drei drei', 'sieben sieben sechs sechs zwei', 'vier vier']
+    const newWords = splitByStopWords(sentences, stopWords)
+
+    it('should not split when no split word ist present and return no white space', () => {
+        expect(newWords).toEqual(['zwei drei drei', 'sieben sieben sechs sechs zwei', 'vier vier'])
     })
 })
