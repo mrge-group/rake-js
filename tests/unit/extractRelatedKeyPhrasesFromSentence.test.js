cb35d522a6981dcb2b304b293b6972da10065746
Kay Gensmann <kay.gensmann@s24.com>
2019-09-19 13:24:45 +0200

    tests splitScentences and removed extractRelatedKey test

--- tests/unit/extractRelatedKeyPhrasesFromSentence.test.js
+++
@@ -1,7 +0,0 @@
-import { extractRelatedKeyPhrasesFromSentence } from '../../src/lib/keywords'
-import mockData from '../__mocks__/mockData'
-
-describe('extractRelatedKeyPhrasesFromSentence', () => {
-    const sentence = mockData.defaultSentence
-    const stopWords = mockData.stopwords
-})
--- tests/unit/splitSentences.test.js
+++ tests/unit/splitSentences.test.js
@@ -7,7 +7,9 @@ describe('splitSentences', () => {
     it('should return an array of strings', () => {
         expect(splitSentences(standard)).toBeInstanceOf(Array)
     })
-    // it('should split at the delimiters and remove them', () => {
-    //     expect(splitSentences(specialChars).toNotContain)
-    // })
+
+    it('should split at the delimiters and remove them', () => {
+        console.log(specialChars)
+        expect(splitSentences(specialChars)).not.toBe(mockData.specialChars)
+    })
 })
