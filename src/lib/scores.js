822549922e30c71e792914e810fa4e965aece2f7
David Kurt <david.kurt@s24.com>
2019-09-20 08:08:04 +0200

    Implemented phrase scoring.

--- src/lib/scores.js
+++ src/lib/scores.js
@@ -1,10 +1,9 @@
 /**
- * Returns an object, whom keys are words and values are the words score. The score is calculated based on the
- * appearance frequency and length of the single phrases we word resides in.
+ * Returns objects within an array including words with their calculated score.
  *
  * @param {String[]} phrases
  *
- * @returns {{score: number, word: String}[]}
+ * @returns {{score: Number, word: String}[]}
  */
 const calculateWordScores = (phrases) => {
     const wordsFrequency = {}
@@ -26,4 +25,34 @@ const calculateWordScores = (phrases) => {
     }))
 }
 
-export { calculateWordScores }
+/**
+ * Returns objects within an array including phrases and their score, accumulated by words and their score.
+ *
+ * @param {String[]}                        phrases
+ * @param {{score: number, word: String}[]} wordsScore
+ * @param {Number}                          minKeywordFrequency
+ *
+ * @returns {{score: Number, phrase: String}[]}
+ */
+const calculatePhraseScores = (phrases, wordsScore, { minKeywordFrequency }) => {
+    let distinctPhrases = [...new Set(phrases)]
+
+    if (minKeywordFrequency > 1) {
+        distinctPhrases = distinctPhrases
+        .filter(distinctPhrase => (phrases.filter(phrase => phrase === distinctPhrase)).length > minKeywordFrequency)
+    }
+
+    return distinctPhrases.map(phrase => {
+        const phraseScore = phrase.split(' ').reduce((accumulator, word) => {
+            const wordScore = wordsScore.filter(wordScore => wordScore.word === word)
+            .reduce((accumulator, wordScore) => accumulator + wordScore.score, 0)
+
+
+            return accumulator + wordScore
+        }, 0)
+
+        return { score: phraseScore, phrase: phrase }
+    })
+}
+
+export { calculateWordScores, calculatePhraseScores }
