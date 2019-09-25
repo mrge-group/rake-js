ae30bbf3391818571c0ed9fd0b2089d4df27b024
David Kurt <david.kurt@s24.com>
2019-09-25 15:45:29 +0200

    Updated default options and added inline docs.

--- src/options.js
+++ src/options.js
@@ -1,13 +1,17 @@
-
-
-const defaultValues = {
+/**
+ * This file includes all overridable rake filtering and scoring options.
+ */
+export default {
+    /** Any keyword or phrase must have this length. Non-compliant will be removed. */
     minCharLength: 1,
+    /** Maximal count of words in a loose keyword phrase. Non-compliant will be removed. */
     maxWordsPerPhrase: 5,
+    /** The frequency a keyword or phrase must be present at scoring. Non-compliant will be removed. */
     minKeywordFrequency: 1,
+    /** Key phrases will have at least this number of keywords. */
     minAdjWordsPerPhrase: 1,
+    /** Key phrases will have only this number of keywords at maximal. */
     maxAdjWordsPerPhrase: 3,
-    minAdjPhraseFreq: 2
+    /** Same as minKeywordFrequency, only applying for key phrases. */
+    minAdjPhraseFreq: 1
 }
-
-export default defaultValues
-export { defaultValues }
