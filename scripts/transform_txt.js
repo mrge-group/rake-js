fe7451485c3e7a4217dbb5c50c34f4f1d53e6880
Kay Gensmann <kay.gensmann@s24.com>
2019-09-26 15:23:57 +0200

    stoplists checks all files in folder

--- scripts/transform_txt.js
+++ scripts/transform_txt.js
@@ -1,26 +1,19 @@
 const fs = require('fs')
 
 const directory = './stopwords/'
+let fileString = ''
+let stopWords = []
 
 fs.readdirSync(directory).forEach(file => {
-    const fileString = fs.readFileSync(`stopwords/${file}`, 'utf8')
-    const stopWords = fileString.split(/\n/)
-
-    fs.writeFile(`dist/${file}.js`, JSON.stringify(stopWords), (err) => {
+    fileString = fs.readFileSync(`stopwords/${file}`, 'utf8')
+    stopWords = fileString.split(/\n/)
+    const output = []
+    for (let i = 0; i < stopWords.length; i++) {
+        if (/\S/.test(stopWords[i])) {
+            output.push((stopWords[i]).trim())
+        }
+    }
+    fs.writeFile(`dist/${file}.js`, JSON.stringify(output), (err) => {
         if (err) throw err
     })
 })
-
-// var lines = $('#input').val().split(/\n/);
-// var output = [];
-// var outputText = [];
-// for (var i = 0; i < lines.length; i++) {
-//     // only push this line if it contains a non whitespace character.
-//     if (/\S/.test(lines[i])) {
-//         outputText.push('"' + $.trim(lines[i]) + '"');
-//         output.push($.trim(lines[i]));
-//     }
-// }
-// console.log(output);
-// $('#input').val('[' + outputText + ']');
-// $('.alert').removeClass('alert-info').addClass('alert-success').text('Done!')
