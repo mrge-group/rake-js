dbb9c90a7953ea30915826e569de0968789ff81e
David Kurt <david.kurt@s24.com>
2019-10-15 12:02:00 +0200

    Added jsdoc to rake distribution file.

--- src/dist.js
+++ src/dist.js
@@ -1,5 +1,17 @@
 import rake from './rake'
 
+/**
+ * Standalone distribution of rake method.
+ *
+ * @param {String}   text
+ *                   The text you want to analyse.
+ * @param {String[]} stopWordsAttribute
+ *                   Stop words used to split and score the text.
+ * @param {Object}   overrideOptions
+ *                   Override default filtering and scoring options.
+ *
+ * @returns  {{score: Number, phrase: String}[]}
+ */
 window.Rake = (text, stopWordsAttribute = [], overrideOptions = {}) => {
     let relevantStopWords = []
 
