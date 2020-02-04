80367f3fe9f7d8e7fbcd5563bf73049c0cac4dcd
Kay Gensmann <kay.gensmann@s24.com>
2020-02-04 11:12:56 +0100

    removed comment

--- src/rake.js
+++ src/rake.js
@@ -18,8 +18,6 @@ const rake = (text, stopWords = [], overrideOptions = {}) => {
 
     const sentences = splitSentences(text)
 
-    // console.log('sentences: ', sentences)
-
     const candidatePhrases = findCandidateKeywords(sentences, stopWords, options)
 
     const wordScores = calculateWordScores(candidatePhrases)
