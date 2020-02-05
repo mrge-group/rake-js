4f8dfa87e9c87e4a2bd896fbdd3c7efbbd0af1d0
David Kurt <david.kurt@s24.com>
2020-02-05 14:11:40 +0100

    Created pipe method calculating word frequency score

---
+++ src/utils/scoreWordFrequency.js
@@ -0,0 +1,40 @@
+import splitWords from '../lib/splitWords'
+import options from '../lib/options'
+
+/**
+ * Calculate word scoring based on word appearance frequency and degree.
+ *
+ * @param {Phrases} phrases
+ *
+ * @returns {Phrases}
+ */
+const scoreWordFrequency = (phrases) => {
+    const { result, original, toPhrase, options: overrides } = phrases
+
+    // Retrieve config.
+    const stopWords = options(overrides).get('stopWords', [])
+
+    const wordsFrequency = {}
+
+    // Calculate words frequency and degree.
+    result.forEach(({ phrase }) => {
+        splitWords(phrase)
+        .forEach(word => {
+            if (stopWords.includes(word.toLowerCase())) {
+                return
+            }
+
+            wordsFrequency[word] = splitWords(original).filter(origWord => origWord.toLowerCase() === word.toLowerCase()).length
+        })
+    })
+
+    phrases.result = result.map(({ phrase, score}) => toPhrase(
+        phrase,
+        score + splitWords(phrase)
+            .reduce((phraseAccumulator, word) => phraseAccumulator + (wordsFrequency[word] || 0), 0)
+    ))
+
+    return phrases
+}
+
+export default scoreWordFrequency
