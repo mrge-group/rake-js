6856cdb1e807b2d47e8e7933fe75a72441ef11cb
Kay Gensmann <kay.gensmann@s24.com>
2020-02-05 17:09:16 +0100

    articles with nouns method

---
+++ src/utils/extractArticlesWithNouns.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieve key phrases splitted by stop words.
+ *
+ * @param {Phrases} phrases
+ *
+ * @returns {Phrases}
+ */
+
+const extractArticlesWithNouns = (phrases) => {
+    return phrases
+}
+
+export default extractArticlesWithNouns
