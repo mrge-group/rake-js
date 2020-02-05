d9357706c566b6487ef240485408fd79b3bdba32
David Kurt <david.kurt@s24.com>
2020-02-05 12:29:05 +0100

    Created distinct filter pipe method

---
+++ src/utils/distinct.js
@@ -0,0 +1,19 @@
+/**
+ * Filters results for only distinct phrases.
+ *
+ * @param {Phrases} phrases
+ *
+ * @returns {Phrases}
+ */
+const distinct = (phrases) => {
+    const { result } = phrases
+
+    phrases.result = result.filter((phrase, phraseIndex, allPhrases) => {
+        return !allPhrases.some((checkPhrase, checkIndex) =>
+                checkPhrase.phrase.toLowerCase() === phrase.phrase.toLowerCase() && checkIndex > phraseIndex)
+    })
+
+    return phrases
+}
+
+export default distinct
