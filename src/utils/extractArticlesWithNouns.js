87e6615985440cde1abc461f17b4c9f93a592d00
Kay Gensmann <kay.gensmann@s24.com>
2020-02-06 15:46:57 +0100

    added digits to regex

--- src/utils/extractArticlesWithNouns.js
+++ src/utils/extractArticlesWithNouns.js
@@ -1,3 +1,5 @@
+import articles from '../stopwords/articles'
+
 /**
  * Retrieve key phrases splitted by stop words.
  *
@@ -8,6 +10,24 @@
 
 const extractArticlesWithNouns = (phrases) => {
     const { result, original, toPhrase, options: overrides } = phrases
+    const srcLowerCase = original.toLowerCase()
+    const returnResult = []
+
+    articles.map(article => {
+        result.map(noun => {
+            const combined = article + ' ' + noun.phrase
+            if (srcLowerCase.includes(combined)) {
+                const nounString = original.substr(srcLowerCase.indexOf(combined), combined.length)
+                if (nounString.match(/\b[A-Z]\S+/g)) {
+                    returnResult.push({
+                        phrase: combined,
+                        score: noun.score
+                    })
+                }
+            }
+        })
+        return returnResult
+    })
     return phrases
 }
 
